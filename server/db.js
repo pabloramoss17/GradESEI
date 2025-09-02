9aa7c87b191d8f55a50d1824becef50e45cf5530
Pablo Ramos Nogueiras <pabloramos1703@gmail.com>
2025-09-02 11:34:41 +0200

    Cambio para railway 5

--- server/db.js
+++ server/db.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
-});
+let db;
+
+if (process.env.MYSQL_URL) {
+    db = mysql.createConnection(process.env.MYSQL_URL);
+    // Si se proporciona MYSQL_URL, se utiliza para la conexión
+} else {
+    db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 3306
+    });
+}
 
 db.connect((err) => {
     if (err) {
